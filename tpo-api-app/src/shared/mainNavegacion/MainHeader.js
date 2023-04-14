bafc29622583c699f7f3a1a5343239b5472f8de8
JuanxIG <91839195+JuanxIG@users.noreply.github.com>
2023-04-14 19:39:06 -0300

    menu hamburguesa

--- tpo-api-app/src/shared/mainNavegacion/MainHeader.js
+++ tpo-api-app/src/shared/mainNavegacion/MainHeader.js
@@ -1,33 +1,37 @@
-import {Link} from 'react-dom'
+import { useRef } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import "./MainHeader.css";
 
-function CollapsibleExample() {
-  return (
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <Link class="navbar-brand" href="#">Navbar</Link>
-    <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <Link class="nav-link active" aria-current="page" href="#">Home</Link>
-        </li>
-        <li class="nav-item">
-          <Link class="nav-link" href="#">Link</Link>
-        </li>
-        <li class="nav-item">
-          <Link class="nav-link disabled">Disabled</Link>
-        </li>
-      </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
-  );
+function Navbar() {
+	const navRef = useRef();
+
+	const showNavbar = () => {
+		navRef.current.classList.toggle(
+			"responsive_nav"
+		);
+	};
+
+	return (
+		<header>
+			<h3>LOGO</h3>
+			<nav ref={navRef}>
+				<a href="/#">Home</a>
+				<a href="/#">My work</a>
+				<a href="/#">Blog</a>
+				<a href="/#">About me</a>
+				<button
+					className="nav-btn nav-close-btn"
+					onClick={showNavbar}>
+					<FaTimes />
+				</button>
+			</nav>
+			<button
+				className="nav-btn"
+				onClick={showNavbar}>
+				<FaBars />
+			</button>
+		</header>
+	);
 }
 
-export default CollapsibleExample;
+export default Navbar;
