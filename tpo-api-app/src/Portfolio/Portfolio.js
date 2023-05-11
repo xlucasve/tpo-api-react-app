28a3a1b12176a3c2545892a46f4998029f4064a0
Lucas <lucasvonelm@gmail.com>
2023-05-11 15:22:45 -0300

    Añadido Boton de Contacto

--- tpo-api-app/src/Portfolio/Portfolio.js
+++ tpo-api-app/src/Portfolio/Portfolio.js
@@ -1,9 +1,13 @@
 import React from "react";
-import "./Portfolio.css";
 import ListadoCertificados from "./components/Certificado/ListadoCertificados";
 import Introduccion from "./components/Introduccion/Introduccion";
 import SobreMi from "./components/SobreMi/SobreMi";
 import Tecnologia from "./components/Tecnologia/Tecnologia";
+
+import { Link } from "react-router-dom";
+import Button from "@mui/material/Button";
+
+import "./Portfolio.css";
 
 const Portfolio = () => {
   return (
@@ -16,6 +20,11 @@ const Portfolio = () => {
       <Tecnologia />
       <h2 id="Certificados">Certificados</h2>
       <ListadoCertificados />
+      <Link to="/contacto" className="link-centrado">
+        <Button variant="outlined" className="bt-contacto">
+          Contactame
+        </Button>
+      </Link>
     </div>
   );
 };
