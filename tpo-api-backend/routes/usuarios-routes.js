13261cf8ac2ff09267c041254bbd0517a9c0ece3
Lucas <lucasvonelm@gmail.com>
2023-06-26 15:24:47 -0300

    Mejorado documentacion Swagger Backend

--- tpo-api-backend/routes/usuarios-routes.js
+++ tpo-api-backend/routes/usuarios-routes.js
@@ -4,72 +4,8 @@ const router = express.Router();
 
 const usuarioController = require("../controllers/usuario-controller");
 
-/**
- * @swagger
- * /api/usuario/signup:
- *    post:
- *      tags:
- *      - Usuario
- *      description: Registra al usuario
- *      parameters:
- *      - name: email
- *        in: link
- *        description: Correo del usuario a registrar
- *        required: true
- *        schema:
- *          type: string
- *          format: string
- *          example: correo@correo.mail
- *      - name: password
- *        in: link
- *        description: Contraseña del usuario a registrar
- *        required: true
- *        schema:
- *          type: string
- *          format: string
- *          example: 123Contraseña123
- *      responses:
- *        '201':
- *          description: Se creo el usuario exitosamente
- *        '422':
- *          description: Inputs invalidos | Ya existe un usuario con ese correo
- *        '500':
- *          description: Error al registrar el usuario
- */
 router.post("/signup", usuarioController.signup);
 
-/**
- * @swagger
- * /api/usuario/login:
- *    post:
- *      tags:
- *      - Usuario
- *      description: Loguear un usuario
- *      parameters:
- *      - name: email
- *        in: link
- *        description: Correo del usuario a registrar
- *        required: true
- *        schema:
- *          type: string
- *          format: string
- *          example: correo@correo.mail
- *      - name: password
- *        in: link
- *        description: Contraseña del usuario a registrar
- *        required: true
- *        schema:
- *          type: string
- *          format: string
- *          example: 123Contraseña123
- *      responses:
- *        '200':
- *          description: Se logueo al usuario exitosamente
- *        '401':
- *          description: Credenciales invalidas
- *        '500':
- *          description: Error al loguearse
- */
 router.post("/login", usuarioController.login);
 
 module.exports = router;
--- tpo-api-backend/swagger.js
+++ tpo-api-backend/swagger.js
@@ -1,32 +1,9 @@
-const swaggerJSDoc = require("swagger-jsdoc");
 const swaggerUi = require("swagger-ui-express");
-
-const options = {
-  definition: {
-    openapi: "3.0.0",
-    info: { title: "Curriculum API", version: "1.0.0" },
-  },
-  components: {
-    securitySchemas: {
-      bearerAuth: {
-        type: "http",
-        scheme: "bearer",
-        bearerFormat: "jwt",
-      },
-    },
-  },
-  security: [
-    {
-      bearerAuth: [],
-    },
-  ],
-  apis: ["routes/contactos-routes.js", "routes/usuarios-routes.js"],
-};
-
-const swaggerSpec = swaggerJSDoc(options);
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
 
 const swaggerDocs = (app, port) => {
-  app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+  app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
   app.get("/docs.json", (req, res) => {
     res.setHeader("Content-Type", "application/json");
     res.send(swaggerSpec);
